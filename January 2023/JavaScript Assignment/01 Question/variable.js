62b74b6abc46f1e0e4b42eee062cea8ed69e2920
nandinichhajed <nandinichhajed08@gmail.com>
2023-04-20 18:34:23 +0530

    01 Question

---
+++ January 2023/JavaScript Assignment/01 Question/variable.js
@@ -0,0 +1,31 @@
+// variables in JS
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let x  = 'Nandini'
+
+let y = true
+let z = false
+
+let m = null
+
+let n = undefined
+var a
+
+let b = 5
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(m);
+console.log(n);
+console.log(a);
+console.log(b);
+
+console.log(`type of x is ${typeof(x)}`);
+console.log(`type of y is ${typeof(y)}`);
+console.log(`type of z is ${typeof(z)}`);
+console.log(`type of m is ${typeof(m)}`);
+console.log(`type of n is ${typeof(n)}`);
+console.log(`type of a is ${typeof(a)}`);
+console.log(`type of b is ${typeof(b)}`);
+
